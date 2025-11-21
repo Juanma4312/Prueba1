7e6ca8604158844a6757c778a63ffe55047edcce
Juanma <juanmanuelsr98@gmail.com>
2025-11-21 12:09:58 +0100

    Añadido println en master

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
         static void main() {
         IO.println(String.format("Hello and welcome!"));
+            System.out.println("Hello and welcome ");
+            System.out.println("println desde master");
         for (int i = 1; i <= 5; i++) {
             IO.println("i = " + i);
         }
